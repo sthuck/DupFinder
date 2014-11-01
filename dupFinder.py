45c63a25855db47284c79ab8ad4b711016f101b9
Aviad Hadad <aviadhadad@gmail.com>
2014-11-01 02:49:35 +0200

    Update dupFinder.py

--- dupFinder.py
+++ dupFinder.py
@@ -3,12 +3,16 @@ __author__ = 'aviad'
 import os
 import hashlib
 import fnmatch
+import re
+
+patterns=['*.pdf','*.epub','*.chm','*.djvu']
+pats = re.compile('|'.join(fnmatch.translate(p) for p in patterns))
 
 d={}
 
 for root, dirs, files in os.walk("."):
     for file in files:
-        if fnmatch.fnmatch(file,'*.pdf'):
+        if pats.match(file):
             FullFile = os.path.join(root,file)
             fd = os.open(FullFile,os.O_RDONLY)
             key = hashlib.sha256(os.read(fd,1000)).hexdigest()
